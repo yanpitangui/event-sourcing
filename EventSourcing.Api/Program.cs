db686e4f9f88c929a6aa1f9d80f62124f9bcfc1c
Yan Pitangui <yanpitangui@gmail.com>
2023-01-29 10:32:28 -0300

    UP: Setup marten and postgres

--- EventSourcing.Api/Program.cs
+++ EventSourcing.Api/Program.cs
@@ -1,5 +1,7 @@
 using FastEndpoints;
 using FastEndpoints.Swagger;
+using Marten;
+using Weasel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,16 @@ builder.Services.AddFastEndpoints(o =>
     o.SourceGeneratorDiscoveredTypes = DiscoveredTypes.All;
 });
 builder.Services.AddSwaggerDoc();
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("DefaultConnection")!);
+
+    if (builder.Environment.IsDevelopment())
+    {
+        options.AutoCreateSchemaObjects = AutoCreate.All;
+    }
+});
 
 var app = builder.Build();
 
